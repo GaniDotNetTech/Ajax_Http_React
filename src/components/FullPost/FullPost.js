e58c0f10679cd3ac480b365c6e5537a66144a391
Ganesh <46371536+GaniDotNetTech@users.noreply.github.com>
2020-02-24 17:10:29 +0530

    Changes from making HTTP request using axios

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,22 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import './FullPost.css';
+import "./FullPost.css";
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
-
-        );
-        return post;
+  state = {
+    loadedPostData: null
+  };
+  componentDidUpdate() {
+    if (this.props.id) {
+      if (
+        !this.state.loadedPostData ||
+        (this.state.loadedPostData &&
+          this.state.loadedPostData.id !== this.props.id)
+      ) {
+        axios.get("/posts/" + this.props.id).then(resposne => {
+          this.setState({ loadedPostData: resposne.data });
+        });
+      }
     }
+  }
+  deleteClickHandler = () => {
+    axios.delete("/posts/" + this.props.id).then(resposne => {
+      console.log(resposne.data);
+    });
+  };
+  render() {
+    let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
+    if (this.props.id) {
+      post = <p style={{ textAlign: "center" }}>Loading...</p>;
+    }
+    if (this.state.loadedPostData) {
+      post = (
+        <div className="FullPost">
+          <h1>{this.state.loadedPostData.title}</h1>
+          <p>{this.state.loadedPostData.body}</p>
+          <div className="Edit">
+            <button onClick={this.deleteClickHandler} className="Delete">
+              Delete
+            </button>
+          </div>
+        </div>
+      );
+    }
+    return post;
+  }
 }
 
 export default FullPost;
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,28 +1,66 @@
-import React, { Component } from 'react';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import React, { Component } from "react";
+import axios from "axios";
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
 
 class Blog extends Component {
-    render () {
+  state = {
+    posts: [],
+    selectedPost: null,
+    error: false
+  };
+  componentDidMount() {
+    axios
+      .get("/posts")
+      .then(resposne => {
+        const posts = resposne.data.slice(0, 4);
+        const updatePost = posts.map(post => {
+          return {
+            ...post,
+            author: "Max"
+          };
+        });
+
+        this.setState({ posts: updatePost });
+      })
+      .catch(err => {
+        this.setState({ error: true });
+      });
+  }
+  selectedClickeHandler = id => {
+    this.setState({ selectedPost: id });
+  };
+
+  render() {
+    let postsData = (
+      <p style={{ textAlign: "centre" }}>Something went Error </p>
+    );
+    if (!this.state.error) {
+      postsData = this.state.posts.map(res => {
         return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
+          <Post
+            key={res.id}
+            title={res.title}
+            Author={res.author}
+            clickHandler={() => this.selectedClickeHandler(res.id)}
+          />
         );
+      });
     }
+    return (
+      <div>
+        <section className="Posts">{postsData}</section>
+        <section>
+          <FullPost id={this.state.selectedPost} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
