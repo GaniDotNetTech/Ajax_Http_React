e93df9e51203af59d365f9034f2640569b12e7c2
Ganesh <46371536+GaniDotNetTech@users.noreply.github.com>
2020-02-27 09:34:42 +0530

    basename to app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Blog from "./containers/Blog/Blog";
 class App extends Component {
   render() {
     return (
+      // <BrowserRouter basename="/myapp"> set default path
       <BrowserRouter>
         <div className="App">
           <Blog />
