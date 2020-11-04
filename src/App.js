1e7e6c61f4115b42a318d8ff5dbae3ea3d8ac876
Ganesh <46371536+GaniDotNetTech@users.noreply.github.com>
2020-11-04 17:40:55 +0530

    Changes for redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
-import Blog from "./containers/Blog/Blog";
+
+import Counter from "./containers/Counter/Counter";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      // <BrowserRouter basename="/myapp"> set default path
-      <BrowserRouter>
-        <div className="App">
-          <Blog />
-        </div>
-      </BrowserRouter>
+      <div className="App">
+        <Counter />
+      </div>
     );
   }
 }
